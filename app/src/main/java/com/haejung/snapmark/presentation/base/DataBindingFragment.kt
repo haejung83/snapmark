69a3f049812d5c7dd0ff0020b76a282c14b84873
Hae Jung Kim <haejung83@gmail.com>
2019-07-05 10:52:10 +0900

    Added common classes for abstract of databinding

---
+++ app/src/main/java/com/haejung/snapmark/presentation/base/DataBindingFragment.kt
@@ -0,0 +1,22 @@
+package com.haejung.snapmark.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class DataBindingFragment<T : ViewDataBinding> : Fragment() {
+
+    protected lateinit var viewDataBinding: T
+        private set
+
+    abstract val layoutResId: Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        DataBindingUtil.inflate<T>(inflater, layoutResId, container, false).apply {
+            viewDataBinding = this
+        }.root
+
+}
